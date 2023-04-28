c4a09fb4cc0cbb5c1cbf2ac279795c535c8c2f0e
Miguel Pinto <migueljcpinto@gmail.com>
2023-04-28 12:09:01 +0200

    form page

--- sources/javascript/quiz.js
+++ sources/javascript/quiz.js
@@ -4,3 +4,15 @@ const body = document.querySelector('body');
 toggleButton.addEventListener('click', () => {
   body.classList.toggle('dark');
 });
+
+function toggleAnswer() {
+  const button = document.getElementById("answer-button");
+  const answer = document.getElementById("answer");
+  if (answer.classList.contains("card__answer-hidden")) {
+    answer.classList.remove("card__answer-hidden");
+    button.innerHTML = "Hide Answer";
+  } else {
+    answer.classList.add("card__answer-hidden");
+    button.innerHTML = "Show Answer";
+  }
+}
